df53996885f8a09edface25c5d6425b42e7fcf03
hondaluth <argan.johnson@gmail.com>
2015-12-25 15:47:10 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+#####################################################################################
 
-## Write a short comment describing this function
+## makeCacheMatrix(x) is just saving the inverse matrix to the "inv_matrix" name.
+## cacheSolve(y) will first check if the inv_matrix is the inverse for the matrix 
+## "y", if not it will then find y^-1
+
+## makeCacheMatrix(x) is just saving the inverse matrix to the "inv_matrix" name.
+m <- matrix(c(-1, -2, 1, 1), 2, 2) ## this is to check the functionality
+m2 <-matrix(c(8, 9, 2, 3), 2, 2) ## this is to check the functionality
+
+## Note, if m and m2 are not the same matrix sizes (i.e. 2x2, 3x3, etc.) this will return an error
+## message. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        id_matrix <<- diag(x = 1, nrow(x), ncol(x)) ## Making identiy matrix
+        inv_matrix <<- solve(x, id_matrix) ## finding inverse matrix
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(y) will first check if the inv_matrix is the inverse for the matrix 
+## "y", if not it will then find y^-1
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix()) {
+        if(identical(inv_matrix %*% x, id_matrix)){ ## checking to see if the cached inverse is good
+                print(inv_matrix) 
+        } else {                                ## if cached inverse is not good calculating new one
+                id_matrix2 <- diag(x = 1, nrow(x), ncol(x))
+                inv_matrix2 <<- solve(x, id_matrix2)
+                print(inv_matrix2)        
+                }
 }
+makeCacheMatrix(m)
+cacheSolve(m)
+cacheSolve(m2)
+##---------------------------- end of assignment--------------------------------------
